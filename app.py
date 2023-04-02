82b92efa0d8b689ab9db25ebaf9960f7e440e7df
G. Christos <13831602+G-Christos@users.noreply.github.com>
2023-04-02 20:35:17 +0300

    update game

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, Blueprint, request, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+# from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+import os, random
 
-app = Flask(__name__)
+app = Flask(__name__) # package that contain the application, new app
 db = SQLAlchemy(app) # create database instance
 bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# import database //// --> absolute path VS /// --> relative path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3' # 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 login_manager = LoginManager()
@@ -20,47 +24,72 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(60), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String, nullable=False,   # default="male" check(gender in ('Female', 'Male', 'Other'))
+    age = db.Column(db.Integer, nullable=False)
+    date_signup = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=60)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    submit = SubmitField('Register')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
-        if existing_user_username:
-            raise ValidationError(
-                'That username already exists. Please choose a different one.')
+@app.route('/') # route decorator
+def index():
+    return render_template('index.html')
 
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=60)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    submit = SubmitField('Login')
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    hashed_password = bcrypt.generate_password_hash(password) # 1st appr.
+    remember = True if request.form.get('remember') else False
+    user = User.query.filter_by(email=email).first()
+    if not user or not bcrypt.check_password_hash(user.password, password): # 1st appr.
+        flash('Please check your login details and try again.')
+        return redirect(url_for('login'))
+    login_user(user, remember=remember)
+    return redirect(url_for('dashboard'))
+
+#### built the game here ####
+bag_of_words = {
+    'die Anmerkung': 'simeiosi',
+    'die Auflage': 'stroma',
+    'die Aufklärung': 'eksigisi',
+    'aufschreiben - notieren': 'simeiono',
+    'die Dichtung': 'poiisi',
+    'die Reinheit': 'agnotita',
+    'derentwillen': 'eksaitias tis thelisis tous',
+    'die Neigung - Vorliebe': 'protimisi',
+    'die Poesie': 'poiisi',
+    'umstritten': 'epimaxos',
+    'die Grausamkeit': 'thiriodia'
+}
+current_word = ''
+current_word_translation = ''
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    global current_word, current_word_translation
+    if request.method == 'POST':
+        answer = request.form['answer']
+        if answer.lower() == current_word_translation.lower():
+            result = 'Correct!'
+        else:
+            result = 'Wrong! The correct answer is "{}".'.format(current_word_translation)
+        current_word, current_word_translation = random.choice(list(bag_of_words.items()))
+        return render_template('dashboard.html', word=current_word, result=result)
+    else:
+        current_word, current_word_translation = random.choice(list(bag_of_words.items()))
+        return render_template('dashboard.html', word=current_word)
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -68,19 +97,39 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@ app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+@ app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+@ app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@ app.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+    if user:
+        flash('Email address already exists.')
+        return redirect(url_for('login')) # signup
+    hashed_password = bcrypt.generate_password_hash(password) # 1st appr.
+    new_user = User(email=email, name=name, password=hashed_password) # 1st appr.
+    # new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    db.session.add(new_user)
+    db.session.commit()
+    flash('New account created successfully! Please login.')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-# sqlite database.db
+## Open terminal ##
+# conda activate projects && cd C:\Users\chris\Downloads\flashcard_game && python app.py
+# http://127.0.0.1:5000/
+
+# sqlite3 db.sqlite3
 # select * from user;
+
